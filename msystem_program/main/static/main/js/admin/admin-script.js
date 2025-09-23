b07cc8f350c84aeb2b990f89fd5d1ef6bd7d932c
dyenchristelle <0324-0914@lspu.edu.ph>
2025-09-23 13:19:22 +0800

    Changed file names, added some files

---
+++ msystem_program/main/static/main/js/admin/admin-script.js
@@ -0,0 +1,31 @@
+// for admin 
+// try lang, pero wala pa db kaya di natapos
+// document.addEventListener("DOMContentLoaded", function() {
+//     const admin_login = document.getElementById("admin-login");
+
+//     const form = document.getElementById("rsrvForm");
+//     if (!form) {
+//         console.error("Form not found!");
+//         return;
+//     }
+//     console.log("Form loaded successfully!");
+
+//     if(admin_login) {
+//         admin_login.addEventListener("click", async function (event) {
+//         event.preventDefault();
+
+//         const admin_username = document.getElementById("admin-username")
+//         const admin_pass = document.getElementById("admin-pass")
+//         const errorMessage = document.getElementById("errorMessage");
+
+//         if (!admin_username || !admin_pass) {
+//             errorMessage.style.display = "block";
+//             errorMessage.textContent = "Please fill out both fields.";
+//             return;
+//         } else {
+
+//         }
+//         window.location.href = "homepage.html";
+//         })
+//     }
+// });
